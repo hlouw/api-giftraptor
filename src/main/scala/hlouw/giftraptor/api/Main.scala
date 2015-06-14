d67796f31235b52f234ed41e15b977ebce80702e
Hentie Louw <hentie.louw@gmail.com>
2015-06-14 11:52:53 +0100

    Bind to 0.0.0.0, not localhost.
    This was preventing the service from working in Docker

--- src/main/scala/hlouw/giftraptor/api/Main.scala
+++ src/main/scala/hlouw/giftraptor/api/Main.scala
@@ -17,5 +17,5 @@ object Main extends App {
   val service = system.actorOf(Props[ServiceActor], "giftraptor-api")
 
   implicit val timeout = Timeout(5.seconds)
-  IO(Http) ? Http.Bind(service, interface = "localhost", port = 8080)
+  IO(Http) ? Http.Bind(service, interface = "0.0.0.0", port = 8080)
 }
